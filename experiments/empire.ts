a7937f9355c1e347c1944dcb17d8c3cc66703246
Adrian Kunz <clashsoft@hotmail.com>
2023-05-24 00:09:32 +0200

    feat: Derive types from empire json

---
+++ experiments/empire.ts
@@ -0,0 +1,74 @@
+export interface Translatable {
+  key: string;
+  literal?: boolean;
+  variables?: {
+    key: string | number;
+    value: Translatable;
+  }[];
+}
+
+export interface Species {
+  class: string;
+  portrait: string;
+  species_name: Translatable;
+  species_plural: Translatable;
+  species_adjective: Translatable;
+  species_bio?: string;
+  name_list: string;
+  gender: string;
+  trait: string | string[];
+}
+
+export interface Ruler {
+  gender: string;
+  name: {
+    full_names: Translatable;
+    use_full_regnal_name?: boolean;
+  };
+  ruler_title?: Translatable;
+  portrait: string;
+  texture: number;
+  attachment: number;
+  clothes: number;
+  trait?: string;
+  leader_class: string;
+}
+
+export interface Flag {
+  icon: {
+    category: string;
+    file: string;
+  };
+  background: {
+    category: string;
+    file: string;
+  };
+  colors: string[];
+}
+
+export interface Empire {
+  key: string;
+  ship_prefix: Translatable;
+  species: Species;
+  secondary_species?: Species;
+  name: Translatable;
+  adjective: Translatable;
+  authority: string;
+  government: string;
+  planet_name: Translatable;
+  planet_class: string;
+  system_name: Translatable;
+  initializer: string;
+  graphical_culture: string;
+  city_graphical_culture: string;
+  empire_flag: Flag;
+  ruler: Ruler;
+
+  spawn_as_fallen: boolean;
+  ignore_portrait_duplication: boolean;
+  room: string;
+  spawn_enabled: boolean;
+  ethic: string | string[];
+  civics?: string[];
+  origin: string;
+}
