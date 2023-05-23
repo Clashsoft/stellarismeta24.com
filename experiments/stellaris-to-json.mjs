a7a7d76852175412675bef5c7d9658cda758b509
Adrian Kunz <clashsoft@hotmail.com>
2023-05-23 23:38:46 +0200

    refactor: Helper functions

--- experiments/stellaris-to-json.mjs
+++ experiments/stellaris-to-json.mjs
@@ -13,15 +13,50 @@ fs.writeFileSync('./user_empire_designs_v3.4.back.txt', result);
 
 /**
  * @param writer {Writer}
+ * @param key {string}
+ * @param value {any}
+ */
+function writeKeyValue(writer, key, value) {
+  if (/^[a-zA-Z_]+$/.test(key)) {
+    writer.write_unquoted(key);
+  } else {
+    writer.write_quoted(key);
+  }
+  writer.write_operator('=');
+  writeAny(writer, value);
+}
+
+/**
+ * @param writer {Writer}
+ * @param obj {object}
+ */
+function writeObject(writer, obj) {
+  writer.write_object_start();
+  for (const [key, value] of Object.entries(obj)) {
+    writeKeyValue(writer, key, value);
+  }
+  writer.write_end();
+}
+
+/**
+ * @param writer {Writer}
+ * @param obj {Array}
+ */
+function writeArray(writer, obj) {
+  writer.write_array_start();
+  for (const item of obj) {
+    writeAny(writer, item);
+  }
+  writer.write_end();
+}
+
+/**
+ * @param writer {Writer}
  * @param obj {any}
  */
 function writeAny(writer, obj) {
   if (Array.isArray(obj)) {
-    writer.write_array_start();
-    for (const item of obj) {
-      writeAny(writer, item);
-    }
-    writer.write_end();
+    writeArray(writer, obj);
   } else switch (typeof obj) {
     case 'string':
       writer.write_quoted(obj);
@@ -40,17 +75,7 @@ function writeAny(writer, obj) {
       if (obj instanceof Date) {
         writer.write_date(obj);
       } else if (obj) {
-        writer.write_object_start();
-        for (const [key, value] of Object.entries(obj)) {
-          if (/^[a-zA-Z_]+$/.test(key)) {
-            writer.write_unquoted(key);
-          } else {
-            writer.write_quoted(key);
-          }
-          writer.write_operator('=');
-          writeAny(writer, value);
-        }
-        writer.write_end();
+        writeObject(writer, obj);
       }
       break;
   }
