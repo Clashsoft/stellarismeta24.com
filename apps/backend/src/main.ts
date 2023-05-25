af2fdb0272b81614fcbd8909670de75cf9504195
Adrian Kunz <clashsoft@hotmail.com>
2023-05-25 19:50:22 +0200

    feat(backend): Add Swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,11 +7,21 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('StellarisMeta24.com API')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(globalPrefix, app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
