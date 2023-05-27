6549cd20f4d9dd99751e9d54c296815c373f2005
Adrian Kunz <a.kunz@uni-kassel.de>
2023-05-27 23:56:48 +0200

    fix(web): Use renderer and remove SafeHtml for tooltips

--- apps/web/src/app/directives/tooltip.directive.ts
+++ apps/web/src/app/directives/tooltip.directive.ts
@@ -1,16 +1,16 @@
-import {Directive, HostListener, Inject, Input, OnDestroy} from '@angular/core';
+import {Directive, HostListener, Inject, Input, OnDestroy, Renderer2} from '@angular/core';
 import {DOCUMENT} from "@angular/common";
-import {SafeHtml} from "@angular/platform-browser";
 
 @Directive({
   selector: '[smTooltip]',
 })
 export class TooltipDirective implements OnDestroy {
-  @Input() smTooltip!: string | SafeHtml;
+  @Input() smTooltip!: string;
 
   private container?: HTMLDivElement;
 
   constructor(
+    private renderer: Renderer2,
     @Inject(DOCUMENT) private document: Document,
   ) {
   }
@@ -21,11 +21,11 @@ export class TooltipDirective implements OnDestroy {
       return;
     }
 
-    this.container = this.document.createElement('div');
-    this.container.innerHTML = this.smTooltip.toString();
-    this.container.className = 'tooltip-container';
+    this.container = this.renderer.createElement('div');
+    this.renderer.setProperty(this.container, 'innerHTML', this.smTooltip);
+    this.renderer.addClass(this.container, 'tooltip-container');
     this.updatePosition(event);
-    this.document.body.appendChild(this.container);
+    this.renderer.appendChild(document.body, this.container);
   }
 
   @HostListener('mousemove', ['$event'])
@@ -50,12 +50,12 @@ export class TooltipDirective implements OnDestroy {
     if (!this.container) {
       return;
     }
-    this.document.body.removeChild(this.container);
+    this.renderer.removeChild(document.body, this.container);
     this.container = undefined;
   }
 
   private updatePosition(event: MouseEvent) {
-    this.container!.style.left = `${event.clientX + 12}px`;
-    this.container!.style.top = `${event.clientY + 12}px`;
+    this.renderer.setStyle(this.container!, 'left', `${event.clientX + 12}px`);
+    this.renderer.setStyle(this.container!, 'top', `${event.clientY + 12}px`);
   }
 }
--- apps/web/src/app/localisation/translate.pipe.ts
+++ apps/web/src/app/localisation/translate.pipe.ts
@@ -1,22 +1,15 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {toColor, translate, TranslationKey} from "./translate";
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Pipe({
   name: 'translate',
 })
 export class TranslatePipe implements PipeTransform {
-  constructor(
-    private readonly domSanitizer: DomSanitizer,
-  ) {
-  }
-
-  transform(key: TranslationKey | string): string | SafeHtml {
+  transform(key: TranslationKey | string): string {
     let translated = translate(key);
     while (translated.includes('$')) {
       translated = translated.replace(/\$([\w_]+)\$/g, (_, key) => translate(key));
     }
-    let isHtml = false;
     while (translated.includes('§')) {
       translated = translated.replace(/§([\w_!])/g, (_, key) => {
         if (key === '!') {
@@ -25,11 +18,6 @@ export class TranslatePipe implements PipeTransform {
           return `<span style="color: ${toColor(key)}">`;
         }
       });
-      isHtml = true;
-    }
-
-    if (isHtml) {
-      return this.domSanitizer.bypassSecurityTrustHtml(translated);
     }
 
     return translated;
