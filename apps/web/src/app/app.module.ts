3df33893818ecc88b9c71a7a7b6618c3cdcbdb2b
Adrian Kunz <a.kunz@uni-kassel.de>
2024-06-19 10:16:54 +0200

    chore: Update to Nx 19 / Angular 18

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
-import { EditEmpireComponent } from './edit-empire/edit-empire.component';
-import { TooltipDirective } from './directives/tooltip.directive';
-import { SelectEmpireComponent } from './select-empire/select-empire.component';
-import { HttpClientModule } from '@angular/common/http';
-import { EmpirePreviewComponent } from './empire-preview/empire-preview.component';
-import { StartComponent } from './start/start.component';
-import { EmpireItemComponent } from './empire-item/empire-item.component';
-import { ImportEmpiresComponent } from './import-empires/import-empires.component';
-import { FormsModule } from '@angular/forms';
-import { GalleryComponent } from './gallery/gallery.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {appRoutes} from './app.routes';
+import {EditEmpireComponent} from './edit-empire/edit-empire.component';
+import {TooltipDirective} from './directives/tooltip.directive';
+import {SelectEmpireComponent} from './select-empire/select-empire.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {EmpirePreviewComponent} from './empire-preview/empire-preview.component';
+import {StartComponent} from './start/start.component';
+import {EmpireItemComponent} from './empire-item/empire-item.component';
+import {ImportEmpiresComponent} from './import-empires/import-empires.component';
+import {FormsModule} from '@angular/forms';
+import {GalleryComponent} from './gallery/gallery.component';
 import {NgbDropdownModule, NgbRating} from '@ng-bootstrap/ng-bootstrap';
-import { TranslatePipe } from './localisation/translate.pipe';
+import {TranslatePipe} from './localisation/translate.pipe';
 
 @NgModule({
   declarations: [
@@ -29,15 +29,15 @@ import { TranslatePipe } from './localisation/translate.pipe';
     GalleryComponent,
     TranslatePipe,
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
-        FormsModule,
-        NgbDropdownModule,
-        NgbRating,
-    ],
-  providers: [],
   bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
+    FormsModule,
+    NgbDropdownModule,
+    NgbRating,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
-export class AppModule {}
+export class AppModule {
+}
